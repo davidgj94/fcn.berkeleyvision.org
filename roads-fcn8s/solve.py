7d2e3f6df348527ceaffcb1cfa2a0c8e26881406
davidgj <davidgj@drente.tsc.uc3m.es>
2018-05-17 06:16:45 -0400

    solverstate y guardar loss done

--- roads-fcn8s/solve.py
+++ roads-fcn8s/solve.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import shutil
 import pickle
 import time
+from pathlib import Path
+import parse
 
 try:
     import setproctitle
@@ -15,27 +17,37 @@ try:
 except:
     pass
 
+def get_iter(solver_state):
+    format_string = 'solver_iter_{:0>9}.solverstate'
+    parsed = parse.parse(format_string, solver_state.parts[-1])
+    return int(parsed[0])
+
 results_path = 'results/{}/'.format(sys.argv[1])
+snapshot_dir = "snapshot/{}/".format(sys.argv[1])
 if not os.path.exists(results_path):
     #shutil.rmtree(results_path, ignore_errors=True)
     os.mkdir(results_path)
 #else:
 #    os.mkdir(results_path)
-    
+if not os.path.exists(snapshot_dir):
+    #shutil.rmtree(results_path, ignore_errors=True)
+    os.mkdir(snapshot_dir)
 # init
 caffe.set_mode_gpu()
 
-voc_net = caffe.Net('../voc-fcn8s/deploy.prototxt', '../voc-fcn8s/fcn8s-heavy-pascal.caffemodel', caffe.TRAIN)
-
 solver = caffe.SGDSolver('solver.prototxt')
 
-# surgeries
+p = Path(snapshot_dir)
+states = sorted(list(p.glob('*.solverstate')), key=get_iter)
 
-surgery.transplant(solver.net, voc_net, '_roads');
-
-interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
-print interp_layers
-surgery.interp(solver.net, interp_layers)
+if states:
+    solver.restore('/'.join(states[-1].parts))
+else:
+    voc_net = caffe.Net('../voc-fcn8s/deploy.prototxt', '../voc-fcn8s/fcn8s-heavy-pascal.caffemodel', caffe.TRAIN)
+    # surgeries
+    surgery.transplant(solver.net, voc_net, '_roads');
+    interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+    surgery.interp(solver.net, interp_layers)
 
 # scoring
 val = np.loadtxt('../data/roads/ROADS/ImageSets/Segmentation/val.txt', dtype=str)
@@ -43,9 +55,6 @@ train = np.loadtxt('../data/roads/ROADS/ImageSets/Segmentation/train.txt', dtype
 
 niter = train.shape[0]
 nepoch = 1
-
-print train.shape[0]
-sys.exit()
 
 train_loss = []
 val_loss = []
@@ -55,7 +64,7 @@ iu = []
 if os.path.exists(results_path + 'results.p'):
     with open(results_path + 'results.p', 'rb') as f:
         train_loss , val_loss, acc, iu = pickle.load(f)
-    
+
 for epoch in range(nepoch):
     solver.step(niter)
     train_loss_ = solver.net.blobs['loss'].data
@@ -65,11 +74,26 @@ for epoch in range(nepoch):
     acc.append(acc_)
     iu.append(iu_)
 
-plt.plot(np.arange(len(train_loss)), train_loss)
-plt.show()
-
 with open(results_path + 'results.p', 'wb') as f:
     pickle.dump((train_loss , val_loss, acc, iu), f) 
 
-    
+plt.plot(np.arange(len(train_loss)), train_loss)
+plt.grid()
+plt.title('Train loss')
+plt.show()
+
+plt.plot(np.arange(len(val_loss)), val_loss)
+plt.title('Val loss')
+plt.grid()
+plt.show()
+
+plt.plot(np.arange(len(acc)), acc)
+plt.title('Per Class Accuracy')
+plt.grid()
+plt.show()
+
+plt.plot(np.arange(len(iu)), iu)
+plt.grid()
+plt.title('Iu')
+plt.show()
 
