82fcc45e69902ae1d65d0fed9bfd31fcce2ac635
David <davidgj1994@gmail.com>
2018-06-14 20:58:31 +0200

    create_blended added

---
+++ scripts/create_blended.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import sys
+import numpy as np
+import os
+from pathlib import Path
+import shutil
+import vis
+import pdb
+
+_dataset_dir = 'data/roads/'
+dataset_dir = _dataset_dir + 'ROADS/'
+png_images_dir = dataset_dir + 'PNGImages/'
+segmentation_class_raw_dir = dataset_dir + 'SegmentationClassRaw/'
+save_dir = dataset_dir + 'Blended/'
+
+if os.path.exists(save_dir):
+    shutil.rmtree(save_dir, ignore_errors=True)
+os.makedirs(save_dir)
+pdb.set_trace()
+p = Path(png_images_dir)
+#pdb.set_trace()
+for glob in p.glob("*.png"):
+    img_name = glob.parts[-1]
+    #pdb.set_trace()
+    sat = Image.open(png_images_dir + img_name)
+    label = np.array(Image.open(segmentation_class_raw_dir + img_name))
+    #pdb.set_trace()
+    vis_img = Image.fromarray(vis.vis_seg(sat, label, vis.make_palette(4)))
+    vis_img.save(os.path.join(save_dir, img_name))
+
