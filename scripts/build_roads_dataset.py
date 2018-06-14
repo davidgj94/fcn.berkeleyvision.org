f28e02c0510c8b0d5be77ae00842541061526721
David <davidgj1994@gmail.com>
2018-06-14 20:41:13 +0200

    build_dataset cambiado

--- scripts/build_roads_dataset.py
+++ scripts/build_roads_dataset.py
@@ -9,16 +9,13 @@ import sys
 def get_subdirs(p):
     return [x for x in p.iterdir() if x.is_dir()]
 
-_dataset_dir = '../data/roads/'
-dataset_dir = _dataset_dir + 'ROADS/'
+dataset_dir = 'data/'
 png_images_dir = dataset_dir + 'PNGImages/'
 segmentation_class_dir = dataset_dir + 'SegmentationClass/'
 segmentation_class_raw_dir = dataset_dir + 'SegmentationClassRaw/'
-image_sets_dir = dataset_dir + 'ImageSets/Segmentation/'
+labeled_roads_dir = 'new_roads/'
 
-labeled_roads_dir = sys.argv[1]
-
-if os.path.exists(_dataset_dir):
+if os.path.exists(dataset_dir):
     shutil.rmtree(dataset_dir, ignore_errors=True)
     
 os.makedirs(dataset_dir)
@@ -35,33 +32,40 @@ for road in roads:
         
         disconn_mask_path = '/'.join(glob.parts[:-1]) + '/mask_disconn.png'
         other_mask_path = '/'.join(glob.parts[:-1]) + '/mask_other.png'
+        short_mask_path = '/'.join(glob.parts[:-1]) + '/mask_short.png'
         sat_path = '/'.join(glob.parts)
         
-        if not (os.path.exists(disconn_mask_path) and os.path.exists(other_mask_path)):
-            print disconn_mask_path
+        if not os.path.exists(other_mask_path):
             print 'Masks incompletas en {}'.format(sat_path)
             continue
         
-        disconn_mask_length, disconn_mask_width = np.array(Image.open(disconn_mask_path)).shape[:-1]
-        other_mask_length, other_mask_width = np.array(Image.open(other_mask_path)).shape[:-1]
-        sat_length, sat_width = np.array(Image.open(sat_path)).shape[:-1]
+        sat = np.array(Image.open(sat_path))
+        other_mask = np.array(Image.open(other_mask_path))
         
-        if disconn_mask_length == other_mask_length and disconn_mask_width == other_mask_width:
-            if sat_length != disconn_mask_length and sat_width != disconn_mask_width:
-                print 'Dimensiones sat.png no coinciden en {}'.format(sat_path)
-                continue
+        if sat.shape == other_mask.shape:
+            
+            new_name = ':'.join(glob.parts[-4:-1])
+            shutil.copy(sat_path, png_images_dir + new_name + '.png')
+            new_dir = '{}/{}/'.format(segmentation_class_dir, new_name)
+            os.makedirs(new_dir)
+            shutil.copy(other_mask_path, new_dir + 'other.png')
+            
+            if os.path.exists(disconn_mask_path):
+                disconn_mask = np.array(Image.open(disconn_mask_path))
+                if sat.shape == disconn_mask.shape:
+                    shutil.copy(disconn_mask_path, new_dir + 'disconn.png')
+                else:
+                    print 'Dimensiones no coinciden en {}'.format(sat_path)
+            
+            if os.path.exists(short_mask_path):
+                short_mask = np.array(Image.open(short_mask_path))
+                if sat.shape == short_mask.shape:
+                    shutil.copy(short_mask_path, new_dir + 'short.png')
+                else:
+                    print 'Dimensiones no coinciden en {}'.format(sat_path)
+                    
         else:
-            print 'Dimensiones mask_disconn.png y mask_other.png no coinciden en {}'.format(sat_path)
-            continue
+            print 'Dimensiones no coinciden en {}'.format(sat_path)
+            
         
-        new_name = ':'.join(glob.parts[-4:-1])
-        
-        if new_name == '39.273155_-3.446121:sec_3:sec_3_1':
-            continue
-        
-        shutil.copy(sat_path, png_images_dir + new_name + '.png')
-        new_dir = '{}/{}/'.format(segmentation_class_dir, new_name)
-        os.makedirs(new_dir)
-        shutil.copy(disconn_mask_path, new_dir + 'disconn.png')
-        shutil.copy(other_mask_path, new_dir + 'other.png')
     
