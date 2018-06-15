0f68bf8a50a00d10a0f5fb280e4f142c8bca5275
David <davgalla@ing.uc3m.es>
2018-06-15 13:29:16 +0200

    pacomer

--- roads-fcn8s/net.py
+++ roads-fcn8s/net.py
@@ -2,18 +2,21 @@ import caffe
 from caffe import layers as L, params as P
 from caffe.coord_map import crop
 
-def conv_relu(bottom, nout, ks=3, stride=1, pad=1, split='train'):
+def conv_relu(bottom, nout, ks=3, stride=1, pad=1, split='train', use_bn=False):
     conv = L.Convolution(bottom, kernel_size=ks, stride=stride,
         num_output=nout, pad=pad,
         param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)])
     
     relu = L.ReLU(conv, in_place=True)
     
-    use_global_stats = True
-    if 'train' in split:
-        use_global_stats = False
-    bn = L.BatchNorm(relu, use_global_stats=use_global_stats)
-    scale = L.Scale(bn, scale_param=dict(bias_term=True))
+    bn = None
+    scale = None
+    if use_bn:
+        use_global_stats = True
+        if 'train' in split:
+            use_global_stats = False
+        bn = L.BatchNorm(relu, use_global_stats=use_global_stats)
+        scale = L.Scale(bn, scale_param=dict(bias_term=True))
     
     return conv, relu, bn, scale
 
@@ -70,34 +73,34 @@ def fcn_roads(split, batch_size):
             ntop=2, param_str=str(pydata_params))
 
     # the base net
-    n.conv1_1, n.relu1_1, n.bn1_1, n.scale1_1 = conv_relu(n.data, 64, pad=100, split=split)
-    n.conv1_2, n.relu1_2, n.bn1_2, n.scale1_2 = conv_relu(n.scale1_1, 64, split=split)
-    n.pool1 = max_pool(n.scale1_2)
+    n.conv1_1, n.relu1_1, _, _ = conv_relu(n.data, 64, pad=100, split=split)
+    n.conv1_2, n.relu1_2, _, _ = conv_relu(n.relu1_1, 64, split=split)
+    n.pool1 = max_pool(n.relu1_2)
 
-    n.conv2_1, n.relu2_1, n.bn2_1, n.scale2_1 = conv_relu(n.pool1, 128, split=split)
-    n.conv2_2, n.relu2_2, n.bn2_2, n.scale2_2 = conv_relu(n.scale2_1, 128, split=split)
-    n.pool2 = max_pool(n.scale2_2)
+    n.conv2_1, n.relu2_1, _, _ = conv_relu(n.pool1, 128, split=split)
+    n.conv2_2, n.relu2_2, _, _ = conv_relu(n.relu2_1, 128, split=split)
+    n.pool2 = max_pool(n.relu2_2)
 
-    n.conv3_1, n.relu3_1, n.bn3_1, n.scale3_1 = conv_relu(n.pool2, 256, split=split)
-    n.conv3_2, n.relu3_2, n.bn3_2, n.scale3_2 = conv_relu(n.scale3_1, 256, split=split)
-    n.conv3_3, n.relu3_3, n.bn3_3, n.scale3_3 = conv_relu(n.scale3_2, 256, split=split)
-    n.pool3 = max_pool(n.scale3_3)
+    n.conv3_1, n.relu3_1, _, _ = conv_relu(n.pool2, 256, split=split)
+    n.conv3_2, n.relu3_2, _, _ = conv_relu(n.relu3_1, 256, split=split)
+    n.conv3_3, n.relu3_3, _, _ = conv_relu(n.relu3_2, 256, split=split)
+    n.pool3 = max_pool(n.relu3_3)
 
-    n.conv4_1, n.relu4_1, n.bn4_1, n.scale4_1 = conv_relu(n.pool3, 512, split=split)
-    n.conv4_2, n.relu4_2, n.bn4_2, n.scale4_2 = conv_relu(n.scale4_1, 512, split=split)
-    n.conv4_3, n.relu4_3, n.bn4_3, n.scale4_3 = conv_relu(n.scale4_2, 512, split=split)
-    n.pool4 = max_pool(n.scale4_3)
+    n.conv4_1, n.relu4_1, _, _ = conv_relu(n.pool3, 512, split=split)
+    n.conv4_2, n.relu4_2, _, _ = conv_relu(n.relu4_1, 512, split=split)
+    n.conv4_3, n.relu4_3, _, _ = conv_relu(n.relu4_2, 512, split=split)
+    n.pool4 = max_pool(n.relu4_3)
 
-    n.conv5_1, n.relu5_1, n.bn5_1, n.scale5_1 = conv_relu(n.pool4, 512, split=split)
-    n.conv5_2, n.relu5_2, n.bn5_2, n.scale5_2 = conv_relu(n.scale5_1, 512, split=split)
-    n.conv5_3, n.relu5_3, n.bn5_3, n.scale5_3 = conv_relu(n.scale5_2, 512, split=split)
-    n.pool5 = max_pool(n.scale5_3)
+    n.conv5_1, n.relu5_1, _, _ = conv_relu(n.pool4, 512, split=split)
+    n.conv5_2, n.relu5_2, _, _ = conv_relu(n.relu5_1, 512, split=split)
+    n.conv5_3, n.relu5_3, _, _ = conv_relu(n.relu5_2, 512, split=split)
+    n.pool5 = max_pool(n.relu5_3)
 
     # fully conv
-    n.fc6, n.relu6, n.bn6, n.scale6 = conv_relu(n.pool5, 4096, ks=7, pad=0, split=split)
-    n.drop6 = L.Dropout(n.scale6, dropout_ratio=0.2, in_place=True)
-    n.fc7, n.relu7, n.bn7, n.scale7 = conv_relu(n.drop6, 4096, ks=1, pad=0, split=split)
-    n.drop7 = L.Dropout(n.scale7, dropout_ratio=0.2, in_place=True)
+    n.fc6, n.relu6, _, _ = conv_relu(n.pool5, 4096, ks=7, pad=0, split=split)
+    n.drop6 = L.Dropout(n.relu6, dropout_ratio=0.2, in_place=True)
+    n.fc7, n.relu7, _, _ = conv_relu(n.drop6, 4096, ks=1, pad=0, split=split)
+    n.drop7 = L.Dropout(n.relu7, dropout_ratio=0.2, in_place=True)
     
     # score
 
@@ -129,9 +132,9 @@ def fcn_roads(split, batch_size):
     # pool4 skip
     
     n.score_4p, n.score_51, n.score_52, n.score_53, n.score_pool4_roads = fuse_scores(n.pool4, 
-        n.scale5_1, 
-        n.scale5_2, 
-        n.scale5_3, 
+        n.relu5_1, 
+        n.relu5_2, 
+        n.relu5_3, 
         num_classes)
     
     n.score_pool4c_roads = crop(n.score_pool4_roads, n.upscore2_roads)
@@ -146,9 +149,9 @@ def fcn_roads(split, batch_size):
     # pool3 skip
 
     n.score_3p, n.score_41, n.score_42, n.score_43, n.score_pool3_roads = fuse_scores(n.pool3, 
-        n.scale4_1, 
-        n.scale4_2, 
-        n.scale4_3, 
+        n.relu4_1, 
+        n.relu4_2, 
+        n.relu4_3, 
         num_classes)
     
     n.score_pool3c_roads = crop(n.score_pool3_roads, n.upscore_pool4_roads)
@@ -164,9 +167,9 @@ def fcn_roads(split, batch_size):
     # pool2 skip
 
     n.score_2p, n.score_31, n.score_32, n.score_33, n.score_pool2_roads = fuse_scores(n.pool2, 
-        n.scale3_1, 
-        n.scale3_2, 
-        n.scale3_3, 
+        n.relu3_1, 
+        n.relu3_2, 
+        n.relu3_3, 
         num_classes)
 
     n.score_pool2c_roads = crop(n.score_pool2_roads, n.upscore_pool3_roads)
