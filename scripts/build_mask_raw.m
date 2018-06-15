945b746c421cbfecd55df5f6bfeee13f2549b475
David <davgalla@ing.uc3m.es>
2018-06-15 11:54:54 +0200

    build_mask_raw fixed

--- scripts/build_mask_raw.m
+++ scripts/build_mask_raw.m
@@ -15,6 +15,9 @@ for i = 3:numel(dir_struct)
     if exist(fullfile(root_dir, dir_struct(i).name, 'disconn-short.png'))
         BW_disconn_short = rgb2gray(imread(fullfile(root_dir, dir_struct(i).name, 'disconn-short.png')));
         full_mask(BW_disconn_short == 255) = 3;
+    elseif exist(fullfile(root_dir, dir_struct(i).name, 'short.png'))
+        BW_disconn_short = rgb2gray(imread(fullfile(root_dir, dir_struct(i).name, 'short.png')));
+        full_mask(BW_disconn_short == 255) = 3;   
     end
     disp(strcat(dir_struct(i).name, '.png'));
     imwrite(uint8(full_mask), fullfile(target_dir, strcat(dir_struct(i).name, '.png')));
